6c3f6644e56335ef7554bf7f66dbf7af7557d927
hyeonjin6767 <parkhj067670@gmail.com>
2025-07-29 10:03:36 +0900

    숨겨둔 키 활용

--- SeSACWeek5/ViewController.swift
+++ SeSACWeek5/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .green
-      
+        print(APIKey.kakaoKey) //안에 내용은 깃에서 모름
         
         
         
